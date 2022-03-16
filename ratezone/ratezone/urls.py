9ee2eaaea68af057b9f0f3e410c56092e158b8a4
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-03-16 16:39:55 +0300

    import include

--- ratezone/ratezone/urls.py
+++ ratezone/ratezone/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('main.urls')),
