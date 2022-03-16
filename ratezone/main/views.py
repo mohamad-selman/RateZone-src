dcb2c2222ffb9fc29fdfe7e5020eecbc8da7a4ef
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-03-16 21:29:06 +0300

    Created views functions to display all templates

--- ratezone/main/views.py
+++ ratezone/main/views.py
@@ -9,4 +9,34 @@ from django.contrib.auth import logout, login
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, './index.html')
+
+def searchResults(request):
+    return render(request, './searchResults.html')
+
+def professor(request):
+    return render(request, './professor.html')
+
+def rate(request):
+    return render(request, './rate.html')
+
+def search(request):
+    return render(request, './search.html')
+
+def queue(request):
+    return render(request, './queue.html')
+
+def sign_in(request):
+    return render(request, './signin.html')
+
+def sign_up(request):
+    return render(request, './signup.html')
+
+def course(request):
+    return render(request, './course.html')
+
+def rate_course(request):
+    return render(request, './rateCourse.html')
+
+def dashboard(request):
+    return render(request, './dashboard.html')
