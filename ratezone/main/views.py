627c8a7d15017119fc6f427867d8717a7cd88070
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-03-16 16:36:32 +0300

    views initial commit

--- ratezone/main/views.py
+++ ratezone/main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Course, Department, Faculty, Professor, TeachingAssistant, AuthUser
+from .models import DeptGeneralcomments, FacultyCourse, FacultyMiscellaneous, FacultyPersonality, FacultyWorkload
+from .models import SimilarCourses, SimilarFaculty, UserCourseRev, UserDept, UserFacultyRev
+from django.db import connection
+cursor = connection.cursor()
+from django.contrib.auth import logout, login
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Hello')
