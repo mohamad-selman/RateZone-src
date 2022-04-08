ca7ed08450bf33558d5796b47794b77ccebdb9c5
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-04-08 18:57:01 +0300

    Refactored code

--- ratezone/main/views.py
+++ ratezone/main/views.py
@@ -1,6 +1,3 @@
-import numpy as np
-from sklearn.neighbors import NearestNeighbors
-from sklearn.preprocessing import Normalizer
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db import connections
@@ -18,44 +15,43 @@ def home(request):
     return render(request, './index.html')
 
 
-def searchResults(request):
+def convert_to_dictionary(cursor_description, query_result):
     total_count = 0
-
-    prof_query = "SELECT DISTINCT F.fname, F.lname, D.dept_name, ROUND(F.overall_rating, 2) AS 'overall_rating', F.teaching_quality, F.faculty_id FROM Faculty AS F INNER JOIN Professor AS P ON F.faculty_id = P.faculty_id INNER JOIN Department AS D ON D.dept_code=F.dept_code"
-
-    cursors.execute(prof_query)
-    prof_row = cursors.fetchall()
-    tmp = cursors.description
-    prof = []
-    for r in prof_row:
+    returning_value = []
+    for r in query_result:
         total_count += 1
         i = 0
         d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = r[i]
+        while i < len(cursor_description):
+            d[cursor_description[i][0]] = r[i]
             i += 1
-        prof.append(d)
+        returning_value.append(d)
+    return (returning_value,total_count)
 
-    # for p in prof:
-    #     print(p)
+def searchResults(request):
+    total_count = 0
+
+    # the following is a query that returns all professors
+    # with their desired attributes
+    prof_query = "SELECT DISTINCT F.fname, F.lname, D.dept_name, ROUND(F.overall_rating, 2) AS 'overall_rating', F.teaching_quality, F.faculty_id FROM Faculty AS F INNER JOIN Professor AS P ON F.faculty_id = P.faculty_id INNER JOIN Department AS D ON D.dept_code=F.dept_code"
+
+    # executing the query through connection cursor
+    cursors.execute(prof_query)
+    # cursors return the query result in the form of a tuple
+    # needs to be converted to dictionary-like notation
+    # that is what the loop does
+    prof_row = cursors.fetchall()
+    tmp = cursors.description
+    prof_count = 0
+    (prof, prof_count) = convert_to_dictionary(tmp, prof_row)
 
     ta_query = "SELECT DISTINCT F.faculty_id, fname, lname, dept_name FROM Faculty as F INNER JOIN Teaching_Assistant as T ON F.faculty_id = T.faculty_id INNER JOIN Department AS D ON D.dept_code=F.dept_code"
     cursors.execute(ta_query)
     ta_row = cursors.fetchall()
     tmp = cursors.description
-    ta = []
+    ta_count = 0
+    (ta, ta_count) = convert_to_dictionary(tmp, ta_row)
 
-    for r in ta_row:
-        total_count += 1
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = r[i]
-            i += 1
-        ta.append(d)
-
-    # for t in ta:
-    #     print(t)
 
     # CS query
 
@@ -63,15 +59,9 @@ def searchResults(request):
     cursors.execute(CS_dept_query)
     dept_row = cursors.fetchall()
     tmp = cursors.description
-    CS_dept = []
+    cs_count = 0
+    (CS_dept, cs_count) = convert_to_dictionary(tmp, dept_row)
 
-    for dep in dept_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = dep[i]
-            i += 1
-        CS_dept.append(d)
 
     # CE query
 
@@ -79,15 +69,8 @@ def searchResults(request):
     cursors.execute(CE_dept_query)
     dept_row = cursors.fetchall()
     tmp = cursors.description
-    CE_dept = []
-
-    for dep in dept_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = dep[i]
-            i += 1
-        CE_dept.append(d)
+    ce_count = 0
+    (CE_dept, ce_count) = convert_to_dictionary(tmp, dept_row)
 
     # IS query
 
@@ -95,29 +78,16 @@ def searchResults(request):
     cursors.execute(IS_dept_query)
     dept_row = cursors.fetchall()
     tmp = cursors.description
-    IS_dept = []
+    is_count = 0
+    (IS_dept, is_count) = convert_to_dictionary(tmp, dept_row)
 
-    for dep in dept_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = dep[i]
-            i += 1
-        IS_dept.append(d)
 
     Math_dept_query = "SELECT DISTINCT F.faculty_id, fname, lname, ROUND(F.overall_rating, 2),dept_name FROM Department AS D INNER JOIN Faculty AS F ON D.dept_code=F.dept_code WHERE D.dept_code=410"
     cursors.execute(Math_dept_query)
     dept_row = cursors.fetchall()
     tmp = cursors.description
-    MATH_dept = []
-
-    for dep in dept_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = dep[i]
-            i += 1
-        MATH_dept.append(d)
+    math_count = 0
+    (MATH_dept, math_count) = convert_to_dictionary(tmp, dept_row)
 
 
 
@@ -127,15 +97,8 @@ def searchResults(request):
     cursors.execute(course_query)
     course_row = cursors.fetchall()
     tmp = cursors.description
-    courses = []
-
-    for element in course_row:
-        i = 0
-        c = {}
-        while i < len(tmp):
-            c[tmp[i][0]] = element[i]
-            i += 1
-        courses.append(c)
+    course_count = 0
+    (courses, course_count) = convert_to_dictionary(tmp, course_row)
 
     # print(courses)
 
@@ -143,7 +106,7 @@ def searchResults(request):
 
     tmp1 = Department.objects.all().count()
     tmp2 = Course.objects.all().count()
-    total_count += tmp1 + tmp2
+    total_count = tmp1 + tmp2 + prof_count + ce_count + cs_count + math_count
 
 
 #    print(prof)
@@ -166,46 +129,24 @@ def professor(request, prof_id):
     cursors.execute(prof_query, [prof_id])
     prof_row = cursors.fetchall()
     tmp = cursors.description
-    prof = []
-    for r in prof_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = r[i]
-            i += 1
-        prof.append(d)
-
-    # print(prof)
+    prof_count = 0
+    (prof, prof_count) = convert_to_dictionary(tmp, prof_row)
 
     second_faculty_rev_query = "SELECT COUNT(R.review_id) AS 'rev_count' FROM Faculty AS F INNER JOIN user_faculty_rev AS R ON F.faculty_id=R.faculty_id WHERE F.faculty_id=%s"
     cursors.execute(second_faculty_rev_query, [prof_id])
     prof_row = cursors.fetchall()
     tmp = cursors.description
-    rev_prof = []
-    for r in prof_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = r[i]
-            i += 1
-        rev_prof.append(d)
-
+    (rev_prof, rev_count) = convert_to_dictionary(tmp, prof_row)
     prof[0].update(rev_prof[0])
 
     similar_query = "SELECT F.fname,F.lname, F2.fname,F2.lname, ROUND(F2.overall_rating, 2) AS 'overall_rating' from Faculty F inner join similar_faculty S1 on S1.fid=F.faculty_id inner join Faculty F2 on S1.similar_faculty=F2.faculty_id WHERE F.faculty_id=%s"
     cursors.execute(similar_query, [prof_id])
     sim_row = cursors.fetchall()
     tmp = cursors.description
-    sim_prof = []
-    for r in sim_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = r[i]
-            i += 1
-        sim_prof.append(d)
+    sim_count = 0
+    (sim_prof, sim_count) = convert_to_dictionary(tmp, sim_row)
 
-
+    total_count = sim_count + prof_count + rev_count
 
     result = {
         'prof': prof,
@@ -234,7 +175,7 @@ def queue(request, prof_id=None):
 
     if prof_id is not None:
         print('Before inserting')
-            # UserQueue.objects.create(uid=user_id, fid=faculty_id)
+        # UserQueue.objects.create(uid=user_id, fid=faculty_id)
         query = 'INSERT INTO user_queue VALUES (%s, %s)'
         data = (user_id, faculty_id)
         cursors.execute(query, data)
@@ -246,15 +187,9 @@ def queue(request, prof_id=None):
     cursors.execute(fetch, [user_id])
     prof_row = cursors.fetchall()
     tmp = cursors.description
-    prof = []
-    for r in prof_row:
-        i = 0
-        d = {}
-        while i < len(tmp):
-            d[tmp[i][0]] = r[i]
-            i += 1
-        prof.append(d)
-    print(prof)
+    prof_count = 0
+    (prof, prof_count) = convert_to_dictionary(tmp, prof_row)
+
     result = {
         'professors': prof
     }
