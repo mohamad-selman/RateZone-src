ae1193a231a8052e02c26df15d33b3f3902402e2
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-04-11 08:48:45 +0300

    Improved search

--- ratezone/main/views.py
+++ ratezone/main/views.py
@@ -42,14 +42,22 @@ def searchResults(request):
         print('Here again')
         get_name = request.POST.get('tags')
         print(get_name)
-        L = get_name.split(' ')
-        print(L)
-        query = '''
-                SELECT DISTINCT F.fname, F.lname, D.dept_name, ROUND(F.overall_rating, 2) AS 'overall_rating', 
-                F.teaching_quality, F.faculty_id FROM Faculty AS F INNER JOIN Professor AS P ON F.faculty_id = P.faculty_id 
-                INNER JOIN Department AS D ON D.dept_code=F.dept_code WHERE CONCAT(F.fname, ' ', F.lname) LIKE %s
-                '''
-        cursors.execute(query, [get_name])
+        l = get_name.split(' ')
+        print(l)
+        if len(l) == 1:
+            query = '''
+                    SELECT F.fname, F.lname, D.dept_name, ROUND(F.overall_rating, 2) AS 'overall_rating', 
+                    F.teaching_quality, F.faculty_id FROM Faculty AS F INNER JOIN Professor AS P ON F.faculty_id = P.faculty_id 
+                    INNER JOIN Department AS D ON D.dept_code=F.dept_code WHERE F.fname LIKE %s OR F.lname LIKE %s
+                    '''
+        else:
+            query = '''
+                    SELECT F.fname, F.lname, D.dept_name, ROUND(F.overall_rating, 2) AS 'overall_rating', 
+                    F.teaching_quality, F.faculty_id FROM Faculty AS F INNER JOIN Professor AS P ON F.faculty_id = P.faculty_id 
+                    INNER JOIN Department AS D ON D.dept_code=F.dept_code WHERE CONCAT(F.fname, ' ', F.lname) LIKE %s
+                    '''
+        data = (get_name, get_name)
+        cursors.execute(query, data)
         prof_row = cursors.fetchall()
         tmp = cursors.description
         (prof, total_count) = convert_to_dictionary(tmp, prof_row)
@@ -181,8 +189,68 @@ def professor(request, prof_id):
 
 
 @login_required(login_url='sign_in')
-def rate(request):
-    return render(request, './rate.html')
+def rate(request, prof_id):
+    prof_query = '''
+                    SELECT F.fname, F.lname, ROUND(F.overall_rating,2) AS 'overall_rating',F.teaching_quality,
+                    F.faculty_id,D.dept_name,P.image FROM Faculty AS F INNER JOIN Professor AS P ON 
+                    F.faculty_id=P.faculty_id 
+                    INNER JOIN Department AS D ON F.dept_code=D.dept_code WHERE P.faculty_id = %s
+                 '''
+    cursors.execute(prof_query, [prof_id])
+    prof_row = cursors.fetchall()
+    tmp = cursors.description
+    (prof, prof_count) = convert_to_dictionary(tmp, prof_row)
+    # print(prof)
+    uname = request.user.username
+    print(uname)
+    user = User.objects.get(username=uname)
+    user_id = user.id
+
+    if request.method == 'POST':
+        # D=request.POST['D']
+        quality = request.POST['quality']
+        difficulty = request.POST['difficulty']
+        ratee = request.POST['rate']
+        workload = request.POST.getlist('workload')
+        personality = request.POST.getlist('personality')
+        misc = request.POST.getlist('misc')
+        comment = request.POST['comment']
+        print('id = ', prof_id)
+        # print(D)
+        print("quality = ", quality)
+        print("difficulty = ", difficulty)
+        print("rate = ", rate)
+        print("workload = ", workload)
+        print("personality = ", personality)
+        print("misc = ", misc)
+        print("comment = ", comment)
+
+
+        try:
+            u = UserFacultyRev(faculty_id=prof_id, uid=user_id, overall_rating=ratee,
+                                          difficulty_rating=difficulty, upvotes=0, downvotes=0, report_count=0,
+                                          semester_period='', student_thoughts=comment,
+                                          teaching_quality=quality)
+            u.save()
+            print('Success')
+            # data = (prof_id, user_id, 5, difficulty, comment, quality)
+
+            UserFacultyRev.objects.raw('''
+                INSERT INTO user_faculty_rev (faculty_id, uid, overall_rating, difficulty_rating, student_thoughts, 
+                teaching_quality)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                ''', [prof_id, user_id, 5, difficulty, comment, 4])
+            # data = (prof_id, user_id, 5, difficulty, comment, quality)
+            # cursors.execute(insertion_query, data)
+            print('Success')
+        except:
+            print('Could not review')
+
+    # print(request.POST)
+    result = {
+        'prof': prof
+    }
+    return render(request, './rate.html', result)
 
 
 def search(request):
