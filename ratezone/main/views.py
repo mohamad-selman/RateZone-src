79fd31bcb01a54125b116d6a42b1ad6ab510bd96
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-04-17 09:29:05 +0300

    Refactoring queries

--- ratezone/main/views.py
+++ ratezone/main/views.py
@@ -199,21 +199,34 @@ def professor(request, prof_id):
     # needs to be converted to dictionary-like notation
     # that is what the loop does
     faculty_id = prof_id
-    prof_query = "SELECT DISTINCT F.fname, F.lname, ROUND(F.overall_rating,2) AS 'overall_rating',F.teaching_quality,F.faculty_id,D.dept_name,P.image FROM Faculty AS F INNER JOIN Professor AS P ON F.faculty_id=P.faculty_id INNER JOIN Department AS D ON F.dept_code=D.dept_code WHERE P.faculty_id = %s"
+    prof_query = '''
+                SELECT DISTINCT F.fname, F.lname, ROUND(F.overall_rating,2) AS 'overall_rating',
+                F.teaching_quality,F.faculty_id,D.dept_name,P.image FROM Faculty AS F 
+                INNER JOIN Professor AS P ON F.faculty_id=P.faculty_id INNER JOIN Department AS D 
+                ON F.dept_code=D.dept_code WHERE P.faculty_id = %s
+                '''
     cursors.execute(prof_query, [faculty_id])
     prof_row = cursors.fetchall()
     tmp = cursors.description
     prof_count = 0
     (prof, prof_count) = convert_to_dictionary(tmp, prof_row)
 
-    second_faculty_rev_query = "SELECT COUNT(R.review_id) AS 'rev_count' FROM Faculty AS F INNER JOIN user_faculty_rev AS R ON F.faculty_id=R.faculty_id WHERE F.faculty_id=%s"
+    second_faculty_rev_query = '''
+                            SELECT COUNT(R.review_id) AS 'rev_count' FROM Faculty AS F 
+                            INNER JOIN user_faculty_rev AS R ON F.faculty_id=R.faculty_id 
+                            WHERE F.faculty_id=%s
+                            '''
     cursors.execute(second_faculty_rev_query, [faculty_id])
     prof_row = cursors.fetchall()
     tmp = cursors.description
     (rev_prof, rev_count) = convert_to_dictionary(tmp, prof_row)
     prof[0].update(rev_prof[0])
 
-    similar_query = "SELECT F.fname,F.lname, F2.fname,F2.lname, ROUND(F2.overall_rating, 2) AS 'overall_rating' from Faculty F inner join similar_faculty S1 on S1.fid=F.faculty_id inner join Faculty F2 on S1.similar_faculty=F2.faculty_id WHERE F.faculty_id=%s"
+    similar_query = '''
+                SELECT F.fname,F.lname, F2.fname,F2.lname, ROUND(F2.overall_rating, 2) AS 'overall_rating' 
+                from Faculty F inner join similar_faculty S1 on S1.fid=F.faculty_id 
+                INNER JOIN Faculty F2 on S1.similar_faculty=F2.faculty_id WHERE F.faculty_id=%s
+                '''
     cursors.execute(similar_query, [faculty_id])
     sim_row = cursors.fetchall()
     tmp = cursors.description
