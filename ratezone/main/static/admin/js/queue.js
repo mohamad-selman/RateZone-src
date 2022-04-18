262b9ee299ce41e68a87fdd4012d17159e81f80e
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-04-18 21:45:12 +0300

    Testing the fuseJS search

--- ratezone/main/static/admin/js/queue.js
+++ ratezone/main/static/admin/js/queue.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
 
-    $('#add-to-queue').click(function(event) {
+    $('#addP-to-queue').click(function(event) {
+        alert('You are here')
         event.preventDefault();
         var tmp = $(this).val();
+        alert('ID is ' + tmp);
+        const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+//        const object = document.getElementById('#addP-to-queue')
         $.ajax({
-            url: '/queue/' + tmp,
-            success: function() {
-                alert('Professor added to queue');
+//            object.setRequestHeader('X-CSRFToken', csrftoken);
+            method: "POST",
+            url: '/add_to_queue/' + tmp,
+            headers: {'X-CSRFToken': csrftoken},
+            data: {
+                prof_id: tmp,
+                csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
             },
-            error: function() {
-                alert('Error! Professor already exists in queue');
+            dataType:"html",
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                alert('Professor added to queue: status: ' + data);
+            },
+            error: function(data) {
+                alert('Error! Professor already exists in queue' + data);
             }
         })
     })
