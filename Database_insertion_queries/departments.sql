6422929ab53c56bc1d85214407b9eb8f3ab179ce
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-05-04 02:24:14 +0300

    Added Populate.sql

--- Database_insertion_queries/departments.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Department VALUES (0418, "Computer Science", 0);
-INSERT INTO Department VALUES (1612, "Computer Engineering", 0);
-INSERT INTO Department VALUES (0480, "Statistic", 0);
-INSERT INTO Department VALUES (0410, "Mathematics", 0);
-INSERT INTO Department VALUES (1830, "Information Science", 0);
