ec804db58db847e6cb87ab5ab381c7534ba4e58e
Omar Aly <aly.omar@cs.ku.edu.kw>
2022-05-28 01:37:56 +0300

    Typewriter added

--- ratezone/main/static/admin/js/typewriter.js
+++ ratezone/main/static/admin/js/typewriter.js
@@ -1,10 +1,13 @@
 var ratezone_app = document.getElementById('ratezone_app');
 
 var typewriter = new Typewriter(ratezone_app, {
-    loop: true
+    loop: true,
+    delay: 70
 });
 
 typewriter.typeString('At RateZone, we have hundreds of professors, TAs, and courses ratings')
     .pauseFor(2500)
     .deleteAll()
+    .typeString('Search for a professor, department or course and check out the reviews')
+    .pauseFor(1500)
     .start();
